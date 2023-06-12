44b5b3723ba27af54c9f4a798fd378cfedaa8d01
Marat Khusnetdinov <marat@khusnetdinov.ru>
2023-06-12 22:21:14 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
-git clone https://github.com/khusnetdinov/.spacemacs ~/.spaceemacs.d
+git clone https://github.com/khusnetdinov/.spacemacs ~/.spacemacs.d
